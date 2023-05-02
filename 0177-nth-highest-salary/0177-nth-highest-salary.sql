342c72fe3d1b0bbb7119e345710c42dfa97d26e9
Mustafa Megahed <61358936+Mustafamegahed20@users.noreply.github.com>
2023-05-02 17:34:22 +0300

    Time: 908 ms (16.21%), Space: 0B (100.00%) - LeetHub

--- 0177-nth-highest-salary/0177-nth-highest-salary.sql
+++ 0177-nth-highest-salary/0177-nth-highest-salary.sql
@@ -2,12 +2,11 @@ CREATE FUNCTION getNthHighestSalary(@N INT) RETURNS INT AS
 BEGIN
     RETURN (
         /* Write your T-SQL query statement below. */
-      SELECT DISTINCT Salary
-      FROM Employee e1
-      WHERE @N = (
-      SELECT COUNT(DISTINCT e2.Salary)
-        FROM Employee e2
-      WHERE e2.Salary >= e1.Salary
-        )
+        SELECT DISTINCT Salary
+        FROM (
+          SELECT Salary, DENSE_RANK() OVER (ORDER BY Salary DESC) as rank
+          FROM Employee
+        ) as ranks
+        WHERE rank = @N
     );
 END
