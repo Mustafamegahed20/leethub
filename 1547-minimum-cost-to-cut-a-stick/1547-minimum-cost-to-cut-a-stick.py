8028da8b2ac3b5a3f1f2e556bfcbfeff52249bb6
Mustafa Megahed <61358936+Mustafamegahed20@users.noreply.github.com>
2023-05-28 12:24:22 +0300

    Time: 345 ms (100.00%), Space: 13.7 MB (54.55%) - LeetHub

--- 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.py
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.py
@@ -7,8 +7,8 @@ class Solution(object):
         """
         A = sorted(A + [0, n])
         k = len(A)
-        dp = [[0] * k for _ in xrange(k)]
-        for d in xrange(2, k):
-            for i in xrange(k - d):
-                dp[i][i + d] = min(dp[i][m] + dp[m][i + d] for m in xrange(i + 1, i + d)) + A[i + d] - A[i]
+        dp = [[0] * k for _ in range(k)]
+        for d in range(2, k):
+            for i in range(k - d):
+                dp[i][i + d] = min(dp[i][m] + dp[m][i + d] for m in range(i + 1, i + d)) + A[i + d] - A[i]
         return dp[0][k - 1]
